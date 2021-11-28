4f17d9061aa43f789c0279221bc3a6861386cf44
Кирилл Поздеев <kivillsaw146@gmail.com>
2021-11-28 09:59:58 +0500

    fix

--- frontend/src/boot/axios.ts
+++ frontend/src/boot/axios.ts
@@ -1,5 +1,5 @@
-import { boot } from 'quasar/wrappers'
-import Axios from 'axios'
+import { boot } from 'quasar/wrappers';
+import Axios from 'axios';
 
 // declare module '@vue/runtime-core' {
 //   interface ComponentCustomProperties {
@@ -7,22 +7,23 @@ import Axios from 'axios'
 //   }
 // }
 
-const authToken = localStorage.getItem('token')
-const baseURL = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:3000/api/v1'
-  : `${location.origin}/api/v1`
+const authToken = localStorage.getItem('token');
+const baseURL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000/api/v1'
+    : `${location.origin}/api/v1`;
 let ax = {
-  baseURL: baseURL
-}
+  baseURL: baseURL,
+};
 if (authToken) {
   ax = Object.assign(ax, {
     headers: {
-      Authorization: authToken
-    }
-  })
+      Authorization: authToken,
+    },
+  });
 }
 
-const api = Axios.create(ax)
+const api = Axios.create(ax);
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
@@ -30,9 +31,9 @@ export default boot(({ app }) => {
   // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
   //       so you won't necessarily have to import axios in each vue file
 
-  app.config.globalProperties.$api = api
+  app.config.globalProperties.$api = api;
   // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
   //       so you can easily perform requests against your app's API
-})
+});
 
-export { api, baseURL, authToken }
+export { api, baseURL, authToken };
